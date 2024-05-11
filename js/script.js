f0a6e259f8f12e590fa65ac6c801709695851712
Witty Umosung <64681844+Pukkawit@users.noreply.github.com>
2024-05-11 12:26:44 +0100

    bank-transfer-process

--- js/script.js
+++ js/script.js
@@ -18,10 +18,77 @@ const mediCardInfo = {
   pin: 2222,
   bank: 'Access',
 };
+
+let banks = '1. Access\n2. GTB\n3. FCMB\n4. Fidelity';
+
+function checkInput(input) {
+  // Alphabets only
+  if (/^[a-zA-Z]+$/.test(input)) {
+    return false;
+  }
+  // Alphanumeric characters
+  else if (/^[a-zA-Z0-9]+$/.test(input)) {
+    return false;
+  }
+  // Special characters
+  else if (/^[^\w\s]+$/.test(input)) {
+    return false;
+  }
+  // Input contains a mix of characters
+  else {
+    return false;
+  }
+}
+
+function airtimePurchase() {
+  let myCurrentAccBal;
+  let successfulPurchase;
+  let enteredAmount;
+  let airTimeAmount = Number(
+    prompt('Enter the amount of Airtime you want to purchase: ')
+  );
+
+  if (airTimeAmount >= 100 && airTimeAmount <= 499) {
+    enteredAmount = airTimeAmount;
+    airTimeAmount -= airTimeAmount * 0.02;
+  } else if (airTimeAmount >= 500 && airTimeAmount <= 999) {
+    enteredAmount = airTimeAmount;
+    airTimeAmount -= airTimeAmount * 0.04;
+  } else if (airTimeAmount >= 1000 && airTimeAmount <= 9999) {
+    enteredAmount = airTimeAmount;
+    airTimeAmount -= airTimeAmount * 0.05;
+  } else {
+    enteredAmount = airTimeAmount;
+    airTimeAmount = airTimeAmount - airTimeAmount * (10 / 100);
+  }
+
+  if (airTimeAmount) {
+    myCurrentAccBal = myCardInfo.balance - airTimeAmount;
+    successfulPurchase = prompt(
+      'Thank you for the purchase of N' +
+        enteredAmount +
+        '.\n\nN' +
+        airTimeAmount +
+        ' has been deducted from your account and your current account balance is  N' +
+        myCurrentAccBal +
+        '. \n\nPlease type 0 to exit'
+    );
+    if (successfulPurchase === '0') {
+      alert('Thank you for banking with us. You be the main boss!');
+    } else {
+      alert('Where are you rushing to? .😠');
+    }
+  } else {
+    alert('Thank you for banking with us. You be the main boss!');
+  }
+}
+
 function transferMoney() {
   let receiverAccNo = prompt("Enter the receiver's account number");
-  if (receiverAccNo.length === 10) {
-    let bank = Number(prompt(`Type 1, 2, 3, 4, to choose the bank\n${banks}`));
+  if (checkInput(receiverAccNo) && receiverAccNo.length === 10) {
+    let bank = Number(
+      prompt(`Type 1, 2, 3, or 4, to choose the bank\n${banks}`)
+    );
     if (bank === 1) {
       bank = 'Access';
       if (
@@ -48,7 +115,7 @@ function transferMoney() {
             }
             if (transferAmount === '0' || transferAmount === '') {
               alert(
-                `Chief ${myCardInfo.name}, thank you for banking with us. Your money no reach. Bye!`
+                `Oga ${myCardInfo.name}, thank you for banking with us. Your money no reach. Bye!`
               );
             }
             console.log(myCardInfo.balance > transferAmount);
@@ -83,7 +150,15 @@ function transferMoney() {
                 prompt(`Thank you for banking with us. Bye!`);
               }
             }
+          } else {
+            alert(
+              "Please type an amount next time. If you are not ready, don't initiate transaction. Thank you!"
+            );
           }
+        } else {
+          alert(
+            'Be careful to check wetin you type make you no go lose your money ooo. Thank you for your attempt anyways. Please try again later with carefulness.'
+          );
         }
       } else {
         alert(
@@ -92,27 +167,33 @@ function transferMoney() {
       }
     } else if (bank === 2) {
       bank = 'GTB';
-      alert('GTB');
+      alert('Nothing concern GTB for here. Go and open Access Bank account!');
     } else if (bank === 3) {
       bank = 'FCMB';
-      alert('FCMB');
+      alert('Nothing concern FCMB for here. Go and open Access Bank account!');
     } else if (bank === 4) {
       bank = 'Fidelity';
-      alert('Fidelity');
+      alert(
+        'Nothing concern Fidelity for here. Go and open Access Bank account!'
+      );
     } else {
       alert('The bank is not available');
     }
+  } else {
+    alert(
+      'Account number suppose reach 10 digits or you don type gibberish put inside. Look am well and try again. Thank you!'
+    );
   }
 }
-
-let banks = '1. Access\n2. GTB\n3. FCMB\n4. Fidelity';
 
 const initTransaction = transactionBtn.addEventListener('click', () => {
   let insertCard = prompt('Please insert your card by typing "Yes"');
   if (insertCard.toLowerCase() === 'yes') {
     let confirmPin = Number(prompt('Please enter your pin'));
     if (confirmPin === myCardInfo.pin) {
-      let chooseOption = prompt('Type 1 to Withdraw \nType 2 to Transfer');
+      let chooseOption = prompt(
+        'Type 1 to Withdraw \nType 2 to Transfer\nType 3 to buy airtime'
+      );
       if (chooseOption === '1') {
         let transOrExit = prompt(
           'Service not available. Please type 2 to Transfer or 0 to exit'
@@ -121,9 +202,19 @@ const initTransaction = transactionBtn.addEventListener('click', () => {
           transferMoney();
         } else if (transOrExit === '0') {
           alert('Thank you for banking with us. Bye!');
+        } else {
+          alert(
+            'Where are you rushing to? 😲 Type something and you are rushing carelessly'
+          );
         }
       } else if (chooseOption === '2') {
         transferMoney();
+      } else if (chooseOption === '3') {
+        airtimePurchase();
+      } else {
+        alert(
+          "Thank you for an attempt. You don't have money to do transactions"
+        );
       }
     } else {
       alert('You entered a wrong PIN');
